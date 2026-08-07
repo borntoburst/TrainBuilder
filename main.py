01354aba77573081794f3a53fa6e471facff12f6
borntoburst <minhthangnguyen2109@gmail.com>
2026-08-07 22:16:08 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+TrainBuilder
+Entry Point
+
+Author: Minh Thắng & ChatGPT
+"""
+
+from src.core.app import TrainBuilderApp
+
+
+def main():
+    app = TrainBuilderApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
