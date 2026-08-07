37249cf99240915414488fbd18352b23bb5deee6
borntoburst <minhthangnguyen2109@gmail.com>
2026-08-07 22:39:29 +0700

    Create result_scene.py

---
+++ result_scene.py
@@ -0,0 +1,19 @@
+from src.scenes.base_scene import BaseScene
+
+
+class ResultScene(BaseScene):
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        screen.fill((220, 255, 220))
