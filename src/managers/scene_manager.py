30fb03f4d9172d71babe78ff167408fd5bd09e1e
borntoburst <minhthangnguyen2109@gmail.com>
2026-08-07 23:24:34 +0700

    Update scene_manager.py

--- src/managers/scene_manager.py
+++ src/managers/scene_manager.py
@@ -1,5 +1,4 @@
 """
-TrainBuilder
 Scene Manager
 """
 
@@ -17,61 +16,58 @@ from src.scenes.result_scene import ResultScene
 
 
 class SceneManager:
-    """Quản lý toàn bộ Scene của game."""
 
-    def __init__(self, app):
-        self.app = app
+    def __init__(self, game):
 
-        self.scenes = {}
+        self.game = game
+
         self.current_scene = None
 
-        self._load_scenes()
+        self.scenes = {}
+
+        self._register_scenes()
+
         self.change_scene(SCENE_MENU)
 
-    def _load_scenes(self):
-        """Khởi tạo toàn bộ Scene."""
+    def _register_scenes(self):
 
         self.scenes = {
+
             SCENE_MENU: MenuScene(self),
+
             SCENE_CONFIG: ConfigScene(self),
+
             SCENE_GAMEPLAY: GameplayScene(self),
+
             SCENE_RESULT: ResultScene(self),
+
         }
 
     def change_scene(self, scene_name):
-        """
-        Chuyển sang Scene mới.
-        """
 
         scene = self.scenes.get(scene_name)
 
         if scene is None:
             raise ValueError(f"Scene '{scene_name}' does not exist.")
 
-        # Rời Scene hiện tại
         if self.current_scene is not None:
             self.current_scene.exit()
 
-        # Chuyển Scene
         self.current_scene = scene
 
-        # Khởi tạo Scene mới
         self.current_scene.enter()
 
     def handle_event(self, event):
-        """Chuyển Event cho Scene hiện tại."""
 
-        if self.current_scene is not None:
+        if self.current_scene:
             self.current_scene.handle_event(event)
 
     def update(self, dt):
-        """Cập nhật Scene hiện tại."""
 
-        if self.current_scene is not None:
+        if self.current_scene:
             self.current_scene.update(dt)
 
     def draw(self, screen):
-        """Vẽ Scene hiện tại."""
 
-        if self.current_scene is not None:
+        if self.current_scene:
             self.current_scene.draw(screen)
