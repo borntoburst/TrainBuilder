b720eaf108ab2e73ff08d3547519b01ed04b3aa7
borntoburst <minhthangnguyen2109@gmail.com>
2026-08-07 23:29:37 +0700

    Update game.py

--- src/core/game.py
+++ src/core/game.py
@@ -1,6 +1,6 @@
 """
 TrainBuilder
-Core Application
+Game Core
 """
 
 import pygame
@@ -9,16 +9,28 @@ from src.core.constants import (
     GAME_TITLE,
     WINDOW_SIZE,
     FPS,
+    ASSET_DIR,
 )
 
+from src.managers.asset_manager import AssetManager
 from src.managers.scene_manager import SceneManager
 
 
-class TrainBuilderApp:
+class Game:
 
     def __init__(self):
 
+        # -------------------------------------------------
+        # Initialize pygame
+        # -------------------------------------------------
+
         pygame.init()
+        pygame.font.init()
+        pygame.mixer.init()
+
+        # -------------------------------------------------
+        # Window
+        # -------------------------------------------------
 
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
 
@@ -28,6 +40,13 @@ class TrainBuilderApp:
 
         self.running = True
 
+        # -------------------------------------------------
+        # Managers
+        # -------------------------------------------------
+
+        self.assets = AssetManager(ASSET_DIR)
+        self.assets.load_all()
+
         self.scene_manager = SceneManager(self)
 
     def run(self):
@@ -36,12 +55,7 @@ class TrainBuilderApp:
 
             dt = self.clock.tick(FPS) / 1000
 
-            for event in pygame.event.get():
-
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-                self.scene_manager.handle_event(event)
+            self._handle_events()
 
             self.scene_manager.update(dt)
 
@@ -50,3 +64,19 @@ class TrainBuilderApp:
             pygame.display.flip()
 
         pygame.quit()
+
+    def _handle_events(self):
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                self.running = False
+
+                return
+
+            self.scene_manager.handle_event(event)
+
+    def quit(self):
+
+        self.running = False
