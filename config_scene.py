592d4a49a9ac7e70a7c43eebe17eea296cba0741
borntoburst <minhthangnguyen2109@gmail.com>
2026-08-07 22:38:58 +0700

    Create config_scene.py

---
+++ config_scene.py
@@ -0,0 +1,19 @@
+from src.scenes.base_scene import BaseScene
+
+
+class ConfigScene(BaseScene):
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        screen.fill((240, 240, 240))
